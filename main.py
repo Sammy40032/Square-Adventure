3de24907b9bfc1373a22da194b3415d59d339e01
Sammy40032 <samernestedwards@gmail.com>
2025-03-30 14:58:36 -0400

    Better Movement

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ SCREEN_HEIGHT = 600
 CENTER_Y = SCREEN_HEIGHT // 2
 CENTER_X = SCREEN_WIDTH // 2
 
-# Variables
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 0, 255)
@@ -26,40 +25,50 @@ class Player():
 
         self.vel_y = 0
         self.on_ground = True
+        self.last_jump_time = 0
 
     def render(self):
         pygame.draw.rect(screen, self.colour, (self.x, self.y, self.size, self.size))
 
     def up(self):
         if self.on_ground:
-            self.vel_y = -12  # A smaller negative value for slower jump height
+            self.vel_y = -10
             self.on_ground = False
 
     def move(self):
         self.y += self.vel_y
 
-        # Gravity (slowed down further to make the player fall slower)
         if not self.on_ground:
-            self.vel_y += 0.4  # Slower gravity pull for slower descent
+            self.vel_y += 0.4
 
-        # Prevent falling through the floor
         if self.y >= SCREEN_HEIGHT - self.size:
             self.y = SCREEN_HEIGHT - self.size
             self.vel_y = 0
             self.on_ground = True
 
     def right(self):
-        self.x = self.x + self.speed
+        self.x += self.speed
 
     def left(self):
-        self.x = self.x - self.speed
+        self.x -= self.speed
 
+def check_wall_collision(player):
+    if player.x < 0:
+        player.x = 0
+    if player.x + player.size > SCREEN_WIDTH:
+        player.x = SCREEN_WIDTH - player.size
 
-player = Player(CENTER_X, CENTER_Y, 3, red, 50)  # x, y, speed, colour, size
+player = Player(CENTER_X, SCREEN_HEIGHT - 50 - 50, 5, red, 50) # x, y, speed, colour, size
+
+clock = pygame.time.Clock()
 
 run = True
 while run:
     screen.fill(black)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
     player.move()
     player.render()
@@ -73,9 +82,9 @@ while run:
     if keys[pygame.K_d]:
         player.right()
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+    check_wall_collision(player)
+
+    clock.tick(60)
 
     pygame.display.update()
 
